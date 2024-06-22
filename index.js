5cf3a78a210367fb0900767ef73ddb94dbe73b5e
Mark Njenga <marknjenga25@gmail.com>
2024-06-22 15:55:51 +0300

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,33 @@
+1 + 80;
+60- 40;
+2 * 3.4;
+5.0 / 2.5;
+function add(a, b){
+ return a + b;
+}
+function subtract(a, b){
+ return a - b;
+}
+function multiply(a, b){
+ return a * b;
+}
+function divide(a, b){
+ return a / b;
+}
+function increment(a){
+    return a + 1;
+}
+function decrement(a){
+    return a - 1;
+}
+function makeInt(a){
+    return parseInt(a, 10);
+}
+makeInt("1765");
+makeInt("13.142");
+makeInt("code is cold");
+function preserveDecimal(a){
+    return parseFloat(a);
+}
+preserveDecimal(23.003);
+preserveDecimal("liverpool");
